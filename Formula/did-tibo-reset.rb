05436ec3b11408bc6f88da75562073e8cd20a8a9
Yule Shen <shenyulin.sanshui@bytedance.com>
2026-07-31 00:53:34 +0800

    chore: update did-tibo-reset to 1.4.2

--- Formula/did-tibo-reset.rb
+++ Formula/did-tibo-reset.rb
@@ -5,8 +5,8 @@
 class DidTiboReset < Formula
   desc "Menu bar watcher for Tibo's Codex reset signals"
   homepage "https://github.com/Cirsever/DidTiboReset"
-  url "https://github.com/Cirsever/DidTiboReset/archive/refs/tags/v1.4.1.tar.gz"
-  sha256 "c520b8aa2573df09dd4a96d0a4f73223ec862f094e15808770eb806e341a4bd2"
+  url "https://github.com/Cirsever/DidTiboReset/archive/refs/tags/v1.4.2.tar.gz"
+  sha256 "4f8b12c94f057b4a6c98a3a69ff3daade0762153fe92ed26bacc11f0cdca0ed4"
 
   depends_on macos: :ventura
   depends_on "python@3.13"
